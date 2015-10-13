ad299ed72785a5a07965174a6f06ccc5fd207eb2
unknown <jrussell@cerebro.iVanColo.local>
2015-10-12 23:26:13 -0400

    test: Add first test

--- test/spec/directives/ivh-multi-select.js
+++ test/spec/directives/ivh-multi-select.js
@@ -1,5 +1,35 @@
 
-/**
- * the fire is dead.
- * the room is freezing.
- */
+describe('Directive: ivhMultiSelect', function() {
+  'use strict';
+
+  beforeEach(module('ivh.multiSelect'));
+
+  var ng = angular
+    , $ = jQuery
+    , scope
+    , c; // compile
+
+  beforeEach(inject(function($rootScope, $compile) {
+    scope = $rootScope.$new();
+
+    c = function(tpl, s) {
+      s = s || scope;
+      tpl = ng.isArray(tpl) ? tpl.join('\n') : tpl;
+      var $el = $compile(ng.element(tpl))(s);
+      s.$apply();
+      return $el;
+    };
+  }));
+
+  it('should create a button with text as the transcluded content', function() {
+    var $el = c([
+      '<div ivh-multi-select',
+          'ivh-multi-select-items="[]">',
+        'Blargus',
+      '</div>'
+    ]);
+
+    var tText = $el.text().trim();
+    expect(tText).toBe('Blargus');
+  });
+});
